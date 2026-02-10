3026da906a60c7a6d08eae437db870d32be99208
Daniel Block <uzi@Daniels-MacBook-Air.local>
2026-02-09 20:34:06 -0500

    add optional workoutDate input

--- workout-tracker/src/main/java/com/pxbzi/workout_tracker/workouts/models/NewWorkoutDto.java
+++ workout-tracker/src/main/java/com/pxbzi/workout_tracker/workouts/models/NewWorkoutDto.java
@@ -1,9 +1,11 @@
 package com.pxbzi.workout_tracker.workouts.models;
 
 import com.pxbzi.workout_tracker.workout_sets.models.NewSetDto;
-
+import java.time.LocalDate;
 import java.util.List;
 
-public record NewWorkoutDto(Long exerciseId, List<NewSetDto> sets) {
-
-}
+public record NewWorkoutDto(
+    Long exerciseId,
+    List<NewSetDto> sets,
+    LocalDate workoutDate
+) {}
--- workout-tracker/src/main/java/com/pxbzi/workout_tracker/workouts/models/Workout.java
+++ workout-tracker/src/main/java/com/pxbzi/workout_tracker/workouts/models/Workout.java
@@ -3,19 +3,20 @@ package com.pxbzi.workout_tracker.workouts.models;
 import com.pxbzi.workout_tracker.exercises.models.Exercise;
 import com.pxbzi.workout_tracker.workout_sets.models.WorkoutSet;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(indexes = {
+@Table(
+    indexes = {
         @Index(name = "idx_workout_exercise_id", columnList = "exercise_id"),
         @Index(name = "idx_workout_date", columnList = "workout_date"),
-})
+    }
+)
 @Data
 @NoArgsConstructor
 public class Workout {
@@ -31,7 +32,11 @@ public class Workout {
     @Column(name = "workout_date")
     private LocalDate workoutDate;
 
-    @OneToMany(mappedBy = "workout", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(
+        mappedBy = "workout",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
     private List<WorkoutSet> workoutSets;
 
     private LocalDateTime createdAt;
@@ -39,7 +44,9 @@ public class Workout {
 
     @PrePersist
     public void prePersist() {
-        this.workoutDate = LocalDate.now(ZoneId.of("America/New_York"));
+        if (workoutDate == null) {
+            workoutDate = LocalDate.now(ZoneId.of("America/New_York"));
+        }
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
